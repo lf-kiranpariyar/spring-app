722175f3e3a2a707d4861742582b97bf41494ab9
lf-kiranpariyar <kiranpariyar@lftechnology.com>
2016-03-27 13:50:42 +0545

    created spring boot application

---
+++ src/main/java/com/kiranpariyar/spring/app/SpringAnotationApplication.java
@@ -0,0 +1,12 @@
+package com.kiranpariyar.spring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAnotationApplication.class, args);
+	}
+}
